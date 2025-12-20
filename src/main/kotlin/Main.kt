6f81fceac33d4b48f3db9a9d1c5fbe7fc893a819
Roto90 <slalala90@gmail.com>
2025-12-20 14:37:20 +0900

    feat: [박태준] 자바 -> 코틀린 00

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
 package com
 
 fun main() {
-    val name = "Kotlin"
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        println("i = $i")
-    }
 }
---
+++ src/main/kotlin/example/Lecture00.kt
@@ -0,0 +1,30 @@
+package com.example
+
+class Person(val name: String)
+
+fun main() {
+
+    // 모든 변수는 var / val 을 붙여야 한다.
+    var number1 = 10L
+    // number1 = 5L      // 수정 가능
+
+    val number2 = 10L
+    // number2 = 5L      // 수정 불가능[Read-Only]
+
+    // 자바에서는 long(primitive)과 Long(reference)을 구분하지만 코틀린에서는 구분하지 않음
+    // 코틀린에서는 Long 하나로 쓰지만 내부적으로 추론을 함
+    var number3: Long = 1000L
+    var number4: Long = 2000L
+
+    // nullable 변수 설정 = 변수에 null이 들어가면 타입에 [?] 을 추가해야함
+    var number5: Long? = 1_000L
+    number5 = null
+
+    var person = Person("taejun")
+    println(number1)
+    println(number2)
+    println(number3)
+    println(number4)
+    println(number5)
+    println(person.name)
+}
